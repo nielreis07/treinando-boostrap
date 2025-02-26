7564efad858d1f36a60942cb6a670981ed06dcf3
Daniel Victor <danielvictorreis26@gmail.com>
2025-02-26 19:31:05 -0300

    trocando o nome da file

---
+++ main.php
@@ -0,0 +1,54 @@
+<?php
+ 
+ $carros = [
+    [
+        "id" => 1,
+        "modelo" => "Chevrolet Equinox",
+        "marca" => "Chevrolet",
+        "ano" => 2022,
+        "preco_diaria" => 230.00,
+        "disponivel" => true
+    ],
+    [
+        "id" => 2,
+        "modelo" => "Chevrolet S10",
+        "marca" => "Chevrolet",
+        "ano" => 2021,
+        "preco_diaria" => 250.00,
+        "disponivel" => false
+    ],
+    [
+        "id" => 3,
+        "modelo" => "Chevrolet Onix",
+        "marca" => "Chevrolet",
+        "ano" => 2023,
+        "preco_diaria" => 120.00,
+        "disponivel" => true
+    ],
+    [
+        "id" => 4,
+        "modelo" => "Tracker",
+        "marca" => "Chevrolet",
+        "ano" => 2020,
+        "preco_diaria" => 180.00,
+        "disponivel" => true
+    ],
+    [
+        "id" => 5,
+        "modelo" => "Chevrolet Cruze",
+        "marca" => "Chevrolet",
+        "ano" => 2019,
+        "preco_diaria" => 200.00,
+        "disponivel" => true
+    ],
+    [
+        "id" => 6,
+        "modelo" => "Chevrolet Spin",
+        "marca" => "Chevrolet",
+        "ano" => 2021,
+        "preco_diaria" => 150.00,
+        "disponivel" => true
+    ]
+];
+
+print_r($carros);
